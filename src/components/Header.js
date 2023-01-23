6143a85858243a07f445db0cfcc7200fc6a679ee
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-01-23 14:24:02 +0600

    added nav drawer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import { FiMenu } from "react-icons/fi";
+import { CiLogin } from "react-icons/ci";
 import { Link, NavLink } from "react-router-dom";
+// import component
+import Drawer from 'react-modern-drawer'
+//import styles
+import 'react-modern-drawer/dist/index.css'
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleDrawer = () => {
+      setIsOpen((prevState) => !prevState);
+    };
+
   return (
     <>
       <div class="z-[100] absolute w-full pt-10 px-5 lg:px-0">
@@ -11,13 +22,13 @@ const Header = () => {
           <div class=" grid items-center grid-cols-2 lg:grid-cols-3">
             <ul class="items-center hidden space-x-8 lg:flex">
               <li>
-                <NavLink className='text-lg font-medium'>Home</NavLink>
+                <NavLink className="text-lg font-medium">Home</NavLink>
               </li>
               <li>
-                <NavLink className='text-lg font-medium'>Blog</NavLink>
+                <NavLink className="text-lg font-medium">Blog</NavLink>
               </li>
               <li>
-                <NavLink className='text-lg font-medium'>Course</NavLink>
+                <NavLink className="text-lg font-medium">Course</NavLink>
               </li>
             </ul>
             <a
@@ -50,117 +61,38 @@ const Header = () => {
                 <Link className="text-lg font-medium mr-5">Log In</Link>
               </li>
               <li>
-                <Link className=" text-white bg-black/90 py-3 px-4 rounded-full">Sign Up</Link>
+                <Link className=" text-white bg-black/90 py-3 px-4 rounded-full">
+                  Sign Up
+                </Link>
               </li>
             </ul>
             <div class="ml-auto lg:hidden">
               <button
-                aria-label="Open Menu"
-                title="Open Menu"
-                class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
-                onClick={() => setIsMenuOpen(true)}
+                onClick={toggleDrawer}
               >
-                <FiMenu />
+                <FiMenu className="text-2xl text-gray-800" />
               </button>
-              {isMenuOpen && (
-                <div class="absolute top-0 left-0 w-full">
-                  <div class="p-5 bg-white border rounded shadow-sm">
-                    <div class="flex items-center justify-between mb-4">
-                      <div>
-                        <a
-                          href="/"
-                          aria-label="Company"
-                          title="Company"
-                          class="inline-flex items-center"
-                        >
-                          <svg
-                            class="w-8 text-deep-purple-accent-400"
-                            viewBox="0 0 24 24"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeMiterlimit="10"
-                            stroke="currentColor"
-                            fill="none"
-                          >
-                            <rect x="3" y="1" width="7" height="12" />
-                            <rect x="3" y="17" width="7" height="6" />
-                            <rect x="14" y="1" width="7" height="6" />
-                            <rect x="14" y="11" width="7" height="12" />
-                          </svg>
-                          <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                            Company
-                          </span>
-                        </a>
-                      </div>
-                      <div>
-                        <button
-                          aria-label="Close Menu"
-                          title="Close Menu"
-                          class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                          onClick={() => setIsMenuOpen(false)}
-                        >
-                          <FiMenu />
-                        </button>
-                      </div>
-                    </div>
-                    <nav>
-                      <ul class="space-y-4">
-                        <li>
-                          <a
-                            href="/"
-                            aria-label="Our product"
-                            title="Our product"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Product
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            aria-label="Our product"
-                            title="Our product"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Features
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            aria-label="Product pricing"
-                            title="Product pricing"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Pricing
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            aria-label="Sign in"
-                            title="Sign in"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Sign in
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                            aria-label="Sign up"
-                            title="Sign up"
-                          >
-                            Sign up
-                          </a>
-                        </li>
-                      </ul>
-                    </nav>
+              <Drawer
+                open={isOpen}
+                onClose={toggleDrawer}
+                direction="right"
+                className=""
+                size="50%"
+              >
+                <div className="bg-gray-800 h-full w-full pr-5 py-10">
+                  <div className="ml-5">
+                    <CiLogin
+                      onClick={toggleDrawer}
+                      className="text-[#FFCD70] text-2xl"
+                    />
+                  </div>
+                  <div className="flex flex-col items-end gap-2">
+                    <NavLink className='text-white'>Home</NavLink>
+                    <NavLink className='text-white'>Blog</NavLink>
+                    <NavLink className='text-white'>Course</NavLink>
                   </div>
                 </div>
-              )}
+              </Drawer>
             </div>
           </div>
         </div>
