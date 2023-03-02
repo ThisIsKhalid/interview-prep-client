dd44503672810847ed474a42186215f109753b3d
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-03-02 10:13:15 +0600

    added search nav

--- src/pages/Blog/Blogs.js
+++ src/pages/Blog/Blogs.js
@@ -5,20 +5,23 @@ const Blogs = () => {
   return (
     <>
       {/* search nav */}
-      <div className="bg-gray-700 py-4 flex items-center justify-center px-5">
-        <div className="lg:w-1/2 w-full flex items-center justify-center relative">
+      <div className="bg-gray-700 py-4 flex items-center justify-between px-5">
+        <div className="lg:w-1/2 w-full relative">
           <input
             type="text"
-            className="w-4/5 py-2 rounded-lg px-7 focus:outline-red-500 focus:outline-double focus:outline-2 bg-gray-200 text-gray-900 font-medium"
+            className="w-4/5 py-2 rounded-lg px-7 focus:outline-[#FFCD70] focus:outline-double focus:outline-2 bg-gray-200 text-gray-900 font-medium"
             placeholder="search here"
           />
-          <div className="w-12 bg-cyan-600 h-10 absolute top-0 lg:right-[66px] right-[33px] rounded-r-lg"></div>
+          <div className="w-14 bg-[#FFCD70] h-10 absolute top-0 right-[20%] rounded-r-lg"></div>
           <button>
-            <FiSearch className="absolute top-3 lg:right-20 right-12 text-gray-900 font-semibold" />
+            <FiSearch className="absolute top-3 right-[23%] text-gray-900 text-lg" />
           </button>
         </div>
+        <button className="bg-[#FFCD70] md:py-2 px-5 md:text-base text-sm rounded-lg font-medium text-gray-900">
+          Create Blog
+        </button>
       </div>
-      <div className="pb-20 px-5"></div>;
+      <div className="pb-20 px-5 border border-red-600"></div>
     </>
   );
 };
