051a03ca0faccb534138862791244199e169fa73
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-03-25 19:15:08 +0600

    added preloader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
+import PreLoader from "./components/Loader/PreLoader";
 import { router } from "./routes/route";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 10000);
+
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      {loading ? (
+        <PreLoader />
+      ) : (
+        <RouterProvider router={router}></RouterProvider>
+      )}
     </>
   );
 }
---
+++ src/components/Loader/PreLoader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Watch } from 'react-loader-spinner'
+import './PreLoader.css'
+
+const PreLoader = () => {
+    return (
+      <>
+        <div className="flex flex-col justify-center items-center h-screen text-gray-800 text-2xl">
+          <div class="pencil">
+            <div class="pencil__ball-point"></div>
+            <div class="pencil__cap"></div>
+            <div class="pencil__cap-base"></div>
+            <div class="pencil__middle"></div>
+            <div class="pencil__eraser"></div>
+          </div>
+          <div class="line"></div>
+          <h2 className="text-4xl text-gray-800 font-semibold ">
+            Page Loading...
+          </h2>
+        </div>
+      </>
+    );
+};
+
+export default PreLoader;
